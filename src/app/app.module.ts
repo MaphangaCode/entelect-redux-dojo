830b7e1af8d1fabac68c0e4584a96fd8be3c3ff6
Nkosinathi Maphanga <maphanga.ng@gmail.com>
2020-11-22 12:47:22 +0200

    Added product structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { AddFoodItemComponent } from './add-food-item/add-food-item.component';
+import { FoodItemDisplayComponent } from './food-item-display/food-item-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddFoodItemComponent,
+    FoodItemDisplayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
