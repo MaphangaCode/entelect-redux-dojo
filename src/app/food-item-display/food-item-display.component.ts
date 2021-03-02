edf7331f940986fe994026afdb0ff7829a32d141
Nkosinathi Maphanga <maphanga.ng@gmail.com>
2021-03-03 00:06:07 +0200

    Finished adding previous task code

--- src/app/food-item-display/food-item-display.component.ts
+++ src/app/food-item-display/food-item-display.component.ts
@@ -1,10 +1,24 @@
 import { Component, OnInit } from "@angular/core";
 import { FoodItem } from '../domain/food-item';
+import { select } from '@angular-redux/store';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'food-item-display-app',
     templateUrl: './food-item-display.component.html'
 })
 export class FoodItemDisplayComponent {
+
+    @select('foodItems')
+    foodItemsObs: Observable<FoodItem[]>;
+
     foodItems: FoodItem[] = [];
+
+    // ngOnInit(): void {
+    //     this.foodItemsObs.subscribe((foodItems) => {
+    //         console.log('this ran');
+    //         console.log(foodItems);
+    //         this.foodItems = foodItems;
+    //     });
+    // }
 }
