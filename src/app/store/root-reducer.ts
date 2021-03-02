9e0120d056812797e424dfff0196835bfce0d753
Nkosinathi Maphanga <maphanga.ng@gmail.com>
2021-03-02 23:09:26 +0200

    Added code fill ups from excercise-3

--- src/app/store/root-reducer.ts
+++ src/app/store/root-reducer.ts
@@ -1,6 +1,12 @@
 import { IAppStore } from './store';
 import { RootAction } from './root-action';
+import { ActionType } from './action-type';
 
 export function rootReducer(appState: IAppStore, action: RootAction): IAppStore {
-    return appState;
+    switch(action.type) {
+        case ActionType.ADD_FOOD_ITEM:
+            return { foodItems: [...appState.foodItems, action.data] };
+        default:
+            return appState;    
+    }
 }
